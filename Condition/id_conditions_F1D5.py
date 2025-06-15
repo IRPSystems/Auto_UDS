64468c2fc59644be5dc8c449fd04bb2fb2b3d20d
ilya <ilyar@irpsystems.com>
2025-06-15 09:49:27 +0300

    UDS_Compliance_matrix_UPP has been updated

--- Condition/id_conditions_F1D5.py
+++ Condition/id_conditions_F1D5.py
@@ -1,7 +1,7 @@
 
 
 ID_CONDITIONS = {
-  "Critical CAN Signal Invalid Time  (SCF Maturation)":"00 05 00 00 00 00",
+  "Critical CAN Signal Invalid Time ":"00 05 00 00 00 00",
   "Main CAN Bus Off Healing Time":"00 00 00 64 00 00",
   "CAN Timeout Since Powerup":"00 00 00 00 00 64",
 
