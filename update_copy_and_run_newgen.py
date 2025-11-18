30f1c9c34fd87b6f44a3163f61abcac57ab556e6
ilya <ilyar@irpsystems.com>
2025-11-18 18:10:00 +0200

    added UDS for newGen

--- update_copy_and_run_newgen.py
+++ update_copy_and_run_newgen.py
@@ -107,9 +107,9 @@ def run_parser_for(script_path: Path):
     env["PYTHONPATH"] = os.pathsep.join([p for p in add_paths + [current_pp] if p])
 
     python_exe = base_dir / '.venv' / 'Scripts' / 'python.exe'
-    #cmd = [str(python_exe), "-m", "Project.NewGen.ng", str(script_path)]
-    ng_path = base_dir / 'Project' / 'NewGen' / 'ng.py'
-    cmd = [str(python_exe), str(ng_path), str(script_path)]
+    cmd = [str(python_exe), "-m", "Project.NewGen.ng", str(script_path)]
+    #ng_path = base_dir / 'Project' / 'NewGen' / 'ng.py'
+    #cmd = [str(python_exe), str(ng_path), str(script_path)]
     # Run from the repo root (folder that has the 'Project' package)
     subprocess.run(cmd, check=True, env=env, cwd=str(base_dir))
 
